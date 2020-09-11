54cf0a6b8c96491f6c22f2a0512ea01c4241194d
berger <berger.alexander89@gmail.com>
2020-09-11 19:38:17 +0200

    Updated persistor storage to FlutterStorage

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:redux/redux.dart';                              // new
 import 'package:hmpaisrn/models/app_state.dart';              // new
 import 'package:hmpaisrn/reducers/app_reducer.dart';
 import 'screens/home/index.dart';
-import 'package:hmpaisrn/services/peopleinspace.dart';
 import 'package:redux_thunk/redux_thunk.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'actions/fetch_actions.dart';
@@ -11,6 +10,7 @@ import 'models/app_state.dart';
 import 'dart:io';
 import 'package:redux_persist/redux_persist.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:redux_persist_flutter/redux_persist_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,12 +23,10 @@ void main() async {
     print(e);
   }
 
-  final String path = appDir != null ? appDir.path : "";
-
   final persistor = Persistor<AppState>(
     debug: false,
     throttleDuration: Duration(seconds: 2),
-    storage: FileStorage(File("state.json")), // Or use other engines
+    storage: FlutterStorage(), // Or use other engines
     serializer: JsonSerializer<AppState>(AppState.fromJson), // Or use other serializers
   );
 
